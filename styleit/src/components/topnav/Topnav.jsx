385f3a9bb3d3e744f3496bc44a31fb3ee11b6174
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-12 15:58:25 +0545

    trying to add the search box and button while clicking on the search icon

--- styleit/src/components/topnav/Topnav.jsx
+++ styleit/src/components/topnav/Topnav.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { CgProfile } from 'react-icons/cg'
-import { FiSearch } from 'react-icons/fi'
-import './topnav.css'
+import React, { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { FiSearch } from "react-icons/fi";
+import "./topnav.css";
 
 const Topnav = () => {
-  return (
-<>
-<div className="container top">
-    <CgProfile className='profile_icon'/>
-    <h1>StyleIt</h1>
-    <div className="search">
-    <FiSearch className='search_icon'/>
-    <AiOutlineShoppingCart className='cart_icon'/>
-</div>
-</div>
-</>
-  )
-}
+  const [isShown, setIsShown] = useState(false);
 
-export default Topnav
+  const searchBar = () => {
+    return (
+      <>
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <a href="#" className="btn">
+          Search
+        </a>
+      </>
+    );
+  };
+
+  const handleOnClick = () => {
+    setIsShown(true);
+  };
+
+  return (
+    <>
+      <div className="container top">
+        <CgProfile className="profile_icon" />
+        <h1>StyleIt</h1>
+        <div className="search">
+          <FiSearch className="search_icon" onClick={handleOnClick} />
+          <AiOutlineShoppingCart className="cart_icon" />
+          {isShown && <div className="searchBar">{searchBar()}</div>}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Topnav;
