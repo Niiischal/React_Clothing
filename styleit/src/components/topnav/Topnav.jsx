e1734b148c0885bf57100307ef2dfc9f9a12ff79
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-12 17:56:22 +0545

    removing the previous function which opens the search box on side while clicking the search icon to replace it with offcanvas.

--- styleit/src/components/topnav/Topnav.jsx
+++ styleit/src/components/topnav/Topnav.jsx
@@ -1,45 +1,22 @@
-import React, { useState } from "react";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import { FiSearch } from "react-icons/fi";
-import "./topnav.css";
+import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { CgProfile } from 'react-icons/cg'
+import { FiSearch } from 'react-icons/fi'
+import './topnav.css'
 
 const Topnav = () => {
-  const [isShown, setIsShown] = useState(false);
-
-  const searchBar = () => {
-    return (
-      <>
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <a href="#" className="btn">
-          Search
-        </a>
-      </>
-    );
-  };
-
-  const handleOnClick = () => {
-    setIsShown(!isShown);
-  };
-
   return (
-    <>
-      <div className="container top">
-        <CgProfile className="profile_icon" />
-        <h1>StyleIt</h1>
-        <div className="search">
-          <FiSearch className="search_icon" onClick={handleOnClick} />
-          <AiOutlineShoppingCart className="cart_icon" />
-          {isShown && <div className="searchBar">{searchBar()}</div>}
-        </div>
-      </div>
-    </>
-  );
-};
+<>
+<div className="container top">
+    <CgProfile className='profile_icon'/>
+    <h1>StyleIt</h1>
+    <div className="search">
+    <FiSearch className='search_icon' type='search'/>
+    <AiOutlineShoppingCart className='cart_icon'/>
+</div>
+</div>
+</>
+  )
+}
 
-export default Topnav;
+export default Topnav
