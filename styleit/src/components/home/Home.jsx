b11a76f615b3aeda53e550837e22eb7db61d47b1
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-13 13:49:00 +0545

    trying to make some changes

--- styleit/src/components/home/Home.jsx
+++ styleit/src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import img1 from '../../assets/img1.jpg'
+import img2 from '../../assets/img2.jpg'
 import './home.css'
 
 const Home = () => {
@@ -9,6 +10,14 @@ const Home = () => {
         <div className="first_img">
             <div className="img">
             <img src={img1} alt="" />
+            </div>
+        </div>
+        <div className="contents">
+            <div className="second_img">
+                <img src={img2} alt="" />
+            </div>
+            <div className="description">
+                <h1>Get to know about us</h1>
             </div>
         </div>
         </div>
