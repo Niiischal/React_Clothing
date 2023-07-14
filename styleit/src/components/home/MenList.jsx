26dd42b03819fdee597cb9c2426f3f010e22bcf9
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-14 14:28:00 +0545

    making some changes in the css and adding images to its own item

--- styleit/src/components/home/MenList.jsx
+++ styleit/src/components/home/MenList.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import men1 from '../../assets/men1.jpg'
 import men2 from '../../assets/men2.jpg'
+import men3 from '../../assets/men3.jpg'
+import men4 from '../../assets/men4.jpg'
 
 const MenList = () => {
   return (
-    <div className="accordians">
+    <div className="accordian_first">
     <div className="first_accordian_title">
       <h1>Men.</h1>
-      <h1 className="view">View All></h1>
+      <h5 className="view"><a href="">View All></a></h5>
     </div>
     <div className="items_men">
       <div className="items">
@@ -59,14 +61,14 @@ const MenList = () => {
         >
           <img
             className="card-img-top"
-            src={men1}
+            src={men3}
             alt="Card image cap"
             style={{ height: "25vh" }}
           />
           <div className="card-body">
-            <h5 className="card-title">Summer Knit Shirt</h5>
+            <h5 className="card-title">Brown Corduroy Pants</h5>
             <p></p>
-            <p className="card-text">Rs 3000/-</p>
+            <p className="card-text">Rs 2500/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
@@ -80,14 +82,14 @@ const MenList = () => {
         >
           <img
             className="card-img-top"
-            src={men1}
+            src={men4}
             alt="Card image cap"
             style={{ height: "25vh" }}
           />
           <div className="card-body">
-            <h5 className="card-title">Summer Knit Shirt</h5>
+            <h5 className="card-title">Linen SS Shirt Sand</h5>
             <p></p>
-            <p className="card-text">Rs 3000/-</p>
+            <p className="card-text">Rs 2100/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
