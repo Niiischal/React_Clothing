c7e73c52d978e5df0070e792ab809dab74c12bfd
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-14 19:14:00 +0545

    adding shoe images and its desc

--- styleit/src/components/home/ShoeList.jsx
+++ styleit/src/components/home/ShoeList.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import women1 from '../../assets/women1.jpg'
-import women2 from '../../assets/women2.jpg'
-import women3 from '../../assets/women3.jpg'
-import women4 from '../../assets/women4.jpg'
+import shoe1 from '../../assets/shoe1.jpg'
+import shoe2 from '../../assets/shoe2.jpg'
+import shoe3 from '../../assets/shoe3.jpg'
+import shoe4 from '../../assets/shoe4.jpg'
 
 const ShoeList = () => {
   return (
     <section>
     <div className="accordian">
     <div className="first_accordian_title">
-      <h1>Women</h1>
+      <h1>Shoe</h1>
       <h5 className="view"><a href="">View All></a></h5>
     </div>
     <div className="items_women">
@@ -20,14 +20,14 @@ const ShoeList = () => {
         >
           <img
             className="card-img-top"
-            src={women1}
+            src={shoe1}
             alt="Card image cap"
             style={{ height: "25vh" }}
           />
           <div className="card-body">
-            <h5 className="card-title">Spring Jacket</h5>
+            <h5 className="card-title">Rikki Brown Platform Loafer</h5>
             <p></p>
-            <p className="card-text">Rs 4000/-</p>
+            <p className="card-text">Rs 9000/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
@@ -41,14 +41,14 @@ const ShoeList = () => {
         >
           <img
             className="card-img-top"
-            src={women2}
+            src={shoe2}
             alt="Card image cap"
             style={{ height: "25vh" }}
           />
           <div className="card-body">
-            <h5 className="card-title">Cropped Top</h5>
+            <h5 className="card-title">Nike Dunk Low "Fossil Rose"</h5>
             <p></p>
-            <p className="card-text">Rs 1700/-</p>
+            <p className="card-text">Rs 6700/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
@@ -62,14 +62,14 @@ const ShoeList = () => {
         >
           <img
             className="card-img-top"
-            src={women3}
+            src={shoe3}
             alt="Card image cap"
             style={{ height: "25vh" }}
           />
           <div className="card-body">
-            <h5 className="card-title">Checked Coat</h5>
+            <h5 className="card-title">Nike SB Dunk Low "Pink Box"</h5>
             <p></p>
-            <p className="card-text">Rs 6000/-</p>
+            <p className="card-text">Rs 6700/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
@@ -83,14 +83,14 @@ const ShoeList = () => {
         >
           <img
             className="card-img-top"
-            src={women4}
+            src={shoe4}
             alt="Card image cap"
             style={{ height: "25vh" }}
           />
           <div className="card-body">
-            <h5 className="card-title">Cat-Print Shirt</h5>
+            <h5 className="card-title">Desigual Black Ankle Boots</h5>
             <p></p>
-            <p className="card-text">Rs 2100/-</p>
+            <p className="card-text">Rs 7000/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
