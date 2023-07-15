c75f6a1d439d380ba5e68c54f12b4843d070b2a6
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-15 23:24:29 +0545

    footer completed

--- styleit/src/components/footer/Footer.jsx
+++ styleit/src/components/footer/Footer.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { GrMail } from 'react-icons/gr'
-import { ImLocation } from 'react-icons/im'
-import { MdCall } from 'react-icons/md'
-import './footer.css'
+import React from 'react';
+import { AiFillFacebook, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { GrMail } from 'react-icons/gr';
+import { ImLocation } from 'react-icons/im';
+import { MdCall } from 'react-icons/md';
+import './footer.css';
 
 const Footer = () => {
   return (
@@ -22,7 +23,32 @@ const Footer = () => {
         </ul>
     </div>
     <div className="navigation">
-
+        <h5>Pages</h5>
+        <ul>
+            <li>
+                <a href="/"><p>Home</p></a>
+            </li>
+            <li>
+            <a href="/"><p>Men</p></a>
+            </li>
+            <li>
+            <a href="/"><p>women</p></a>
+            </li>
+            <li>
+            <a href="/"><p>Shoes</p></a>
+            </li>
+            <li>
+            <a href="/"><p>About</p></a>
+            </li>
+        </ul>
+        <div>
+        <h4>&copy; StyleIt. All rights reserved</h4>
+        </div>
+    </div>
+    <div className="medias">
+        <a href=""><AiFillFacebook className='icons'/></a>
+        <a href=""><AiFillInstagram className='icons'/></a>
+        <a href=""><AiFillLinkedin className='icons'/></a>
     </div>
 </footer>
   )
