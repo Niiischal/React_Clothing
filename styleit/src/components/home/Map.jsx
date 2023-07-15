7bcb8d075a6ecf266fedd2d05b4231dec86cdc0c
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-15 23:27:45 +0545

    making some changes in the css.

--- styleit/src/components/home/Map.jsx
+++ styleit/src/components/home/Map.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Map = () => {
   return (
     <section>
+        <h1 style={{textAlign:'center'}}>Find Us Here.</h1>
         <div className="map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7064.758386283339!2d85.32926053811038!3d27.705576203529542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb199ffe9d7c6b%3A0x91b3a969f305a0bc!2sMaitidevi%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1689407211924!5m2!1sen!2snp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
