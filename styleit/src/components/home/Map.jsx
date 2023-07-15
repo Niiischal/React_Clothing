9dc4775c246a65abd5f5562d01d7d17d5f66a3c3
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-15 23:31:20 +0545

    adding a bit presice location on the map.

--- styleit/src/components/home/Map.jsx
+++ styleit/src/components/home/Map.jsx
@@ -5,7 +5,7 @@ const Map = () => {
     <section>
         <h1 style={{textAlign:'center'}}>Find Us Here.</h1>
         <div className="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7064.758386283339!2d85.32926053811038!3d27.705576203529542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb199ffe9d7c6b%3A0x91b3a969f305a0bc!2sMaitidevi%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1689407211924!5m2!1sen!2snp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2313.899444933969!2d85.33308438810785!3d27.706757907533245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb199ffe9d7c6b%3A0x91b3a969f305a0bc!2sMaitidevi%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1689443108961!5m2!1sen!2snp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
   )
