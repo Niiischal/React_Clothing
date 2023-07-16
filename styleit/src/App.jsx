0ba3c8b1abb1ea89714d3af61aff4222fcb60e4a
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-16 16:23:35 +0545

    creating div for the connection and the form content

--- styleit/src/App.jsx
+++ styleit/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Bottomnav from './components/bottomnav/Bottomnav'
+import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Home from './components/home/Home'
 import Topnav from './components/topnav/Topnav'
@@ -8,8 +9,8 @@ const App = () => {
   return (
 <>
 <Topnav/>
-
 <Home/>
+<Contact/>
 <Bottomnav/>
 <Footer/>
 </>
---
+++ styleit/src/components/contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './contact.css'
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <section>
+        <h1>Lets Get Connected</h1>
+        <div className="contact_content">
+          <div className="connection"></div>
+          <div className="form"></div>
+        </div>
+        
+      </section>
+      </div>
+  )
+}
+
+export default Contact
