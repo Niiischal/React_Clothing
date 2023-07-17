76b9d26fa7b0ba1771ce3095bdb95902dc76e95a
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-17 13:17:21 +0545

    adding off canvas while clicking on the profile icon.

--- styleit/src/components/topnav/Topnav.jsx
+++ styleit/src/components/topnav/Topnav.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 import { FiSearch } from 'react-icons/fi'
 import './topnav.css'
 
 const Topnav = () => {
-  const [visible, setVisible] = useState(false)
   return (
 <>
 <div className="container top">
-<CButton className="d-lg-none" onClick={() => setVisible(true)}><a href=""><CgProfile className='profile_icon'/></a></CButton>
-<CAlert className="d-none d-lg-block" color="info">Resize your browser to show the responsive offcanvas toggle.</CAlert>
-    <COffcanvas backdrop="static" placement="start" visible={visible} onHide={() => setVisible(false)}>
-      <COffcanvasHeader>
-        <COffcanvasTitle>Responsive offcanvas</COffcanvasTitle>
-        <CCloseButton className="text-reset" onClick={() => setVisible(false)} />
-      </COffcanvasHeader>
-      <COffcanvasBody>
-        This is content within an <code>.offcanvas-lg</code>.
-      </COffcanvasBody>
-    </COffcanvas>
+  <div>
+    <a data-coreui-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><CgProfile className='profile_icon'/></a>
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <button type="button" class="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+    </div>
+    <div class="dropdown mt-3">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-coreui-toggle="dropdown">
+        Dropdown button
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+    </div>
     <h1>StyleIt</h1>
     <div className="search">
     <a href=""><FiSearch className='search_icon' type='search'/></a>
