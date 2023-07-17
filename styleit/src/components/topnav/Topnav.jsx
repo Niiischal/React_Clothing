de74508883a58642380606a1b47aa5732cc55dae
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-17 13:00:36 +0545

    installing coe ui

--- styleit/src/components/topnav/Topnav.jsx
+++ styleit/src/components/topnav/Topnav.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 import { FiSearch } from 'react-icons/fi'
 import './topnav.css'
 
 const Topnav = () => {
+  const [visible, setVisible] = useState(false)
   return (
 <>
 <div className="container top">
-    <a href=""><CgProfile className='profile_icon'/></a>
+<CButton className="d-lg-none" onClick={() => setVisible(true)}><a href=""><CgProfile className='profile_icon'/></a></CButton>
+<CAlert className="d-none d-lg-block" color="info">Resize your browser to show the responsive offcanvas toggle.</CAlert>
+    <COffcanvas backdrop="static" placement="start" visible={visible} onHide={() => setVisible(false)}>
+      <COffcanvasHeader>
+        <COffcanvasTitle>Responsive offcanvas</COffcanvasTitle>
+        <CCloseButton className="text-reset" onClick={() => setVisible(false)} />
+      </COffcanvasHeader>
+      <COffcanvasBody>
+        This is content within an <code>.offcanvas-lg</code>.
+      </COffcanvasBody>
+    </COffcanvas>
     <h1>StyleIt</h1>
     <div className="search">
     <a href=""><FiSearch className='search_icon' type='search'/></a>
