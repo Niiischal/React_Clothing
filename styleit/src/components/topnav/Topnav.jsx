0e808f778add5f6bf61f67ff3d9276f44ec7eced
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-18 11:51:03 +0545

    creating structure for profile offcanvas

--- styleit/src/components/topnav/Topnav.jsx
+++ styleit/src/components/topnav/Topnav.jsx
@@ -11,14 +11,17 @@ const Topnav = () => {
   <div>
     <a data-coreui-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><CgProfile className='profile_icon'/></a>
 
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" class="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
+    <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <button type="button" className="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
-    <div>
-      ...
+  <div className="offcanvas-body">
+    <div className='profile_photo'>
+      <img src="" alt="" />
+    </div>
+    <div className="profile_content">
+      
     </div>
   </div>
 </div>
@@ -27,12 +30,12 @@ const Topnav = () => {
     <div className="search">
       <div>
     <a data-coreui-toggle="offcanvas" href="#offcanvasExample1" role="button" aria-controls="offcanvasExample1"><FiSearch className='search_icon' type='search'/></a>
-    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasExample1" aria-labelledby="offcanvasExampleLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" class="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
+    <div className="offcanvas offcanvas-top" tabindex="-1" id="offcanvasExample1" aria-labelledby="offcanvasExampleLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <button type="button" className="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
+  <div className="offcanvas-body">
     <div>
       ...
     </div>
@@ -41,12 +44,12 @@ const Topnav = () => {
     </div>
     <div>
     <a data-coreui-toggle="offcanvas" href="#offcanvasExample2" role="button" aria-controls="offcanvasExample2"><AiOutlineShoppingCart className='cart_icon'/></a>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample2" aria-labelledby="offcanvasExampleLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" class="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
+    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample2" aria-labelledby="offcanvasExampleLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <button type="button" className="btn-close text-reset" data-coreui-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
+  <div className="offcanvas-body">
     <div>
       ...
     </div>
