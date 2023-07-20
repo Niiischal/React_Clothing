300313ed6b2e240b8ac7ec4261f1b9f2451dc8fd
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-20 13:18:57 +0545

    creating the required divs and adding the cards for mens components

--- styleit/src/components/men/Men.jsx
+++ styleit/src/components/men/Men.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from "react";
+import "./men.css";
 
 const Men = () => {
   return (
-    <div>
-      Hello Men.
-    </div>
-  )
-}
+    <section>
+      <div className="container Men">
+        <h1>Men's Clothes</h1>
+        <div className="mens_items">
+          <div class="card" style={{width: '15rem'}}>
+            <img class="card-img-top" src="" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{width: '15rem'}}>
+            <img class="card-img-top" src="" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{width: '15rem'}}>
+            <img class="card-img-top" src="" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{width: '15rem'}}>
+            <img class="card-img-top" src="" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{width: '15rem'}}>
+            <img class="card-img-top" src="" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Men
+export default Men;
