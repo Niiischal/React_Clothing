ec143202d12417af68a57bab9728d14f2765194f
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-20 17:30:23 +0545

    adding all the images and description for sneakers list in shoe component

--- styleit/src/components/shoes/Shoes.jsx
+++ styleit/src/components/shoes/Shoes.jsx
@@ -1,93 +1,104 @@
 import React from 'react'
-import women1 from '../../assets/women1.jpg'
-import women10 from '../../assets/women10.jpg'
-import women11 from '../../assets/women11.jpg'
-import women12 from '../../assets/women12.jpg'
-import women2 from '../../assets/women2.jpg'
-import women3 from '../../assets/women3.jpg'
-import women4 from '../../assets/women4.jpg'
-import women5 from '../../assets/women5.jpg'
-import women6 from '../../assets/women6.jpg'
-import women7 from '../../assets/women7.jpg'
-import women8 from '../../assets/women8.jpg'
-import women9 from '../../assets/women9.jpg'
+import shoe11 from '../../assets/img4.jpg'
+import shoe1 from '../../assets/shoe1.jpg'
+import shoe10 from '../../assets/shoe10.jpg'
+import shoe12 from '../../assets/shoe12.jpg'
+import shoe2 from '../../assets/shoe2.jpg'
+import shoe3 from '../../assets/shoe3.jpg'
+import shoe4 from '../../assets/shoe4.jpg'
+import shoe5 from '../../assets/shoe5.jpg'
+import shoe6 from '../../assets/shoe6.jpg'
+import shoe7 from '../../assets/shoe7.jpg'
+import shoe8 from '../../assets/shoe8.jpg'
+import shoe9 from '../../assets/shoe9.jpg'
 import './shoes.css'
 const Shoes = () => {
   return (
     <section>
       <div className="container Shoes">
-      <h1>Shoes Collection</h1>
+      <h1>Sneakers Collection</h1>
         <div className="mens_items">
-          <div className="card" style={{width: '15rem', height:'70vh'}}>
-            <img className="card-img-top" src={women1} alt="" />
+          <div className="card" style={{width: '15rem', height:'75vh'}}>
+            <img className="card-img-top" src={shoe1} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Spring Jacket</h5>
+            <h5 className="card-title">Rikki Brown Platform Loafer</h5>
             <p></p>
-            <p className="card-text">Rs 4000/-</p>
+            <p className="card-text">Rs 9000/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women2} alt="" />
+            <img className="card-img-top" src={shoe2} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Cropped Top</h5>
+            <h5 className="card-title">Nike Dunk Low "Fossil Rose"</h5>
             <p></p>
-            <p className="card-text">Rs 1700/-</p>
+            <p className="card-text">Rs 6700/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women3} alt="" />
+            <img className="card-img-top" src={shoe3} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Checked Coat</h5>
+            <h5 className="card-title">Nike SB Dunk Low "Pink Box"</h5>
             <p></p>
-            <p className="card-text">Rs 6000/-</p>
+            <p className="card-text">Rs 6700/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
-          <div className="card" style={{width: '15rem', height:'70vh'}}>
-            <img className="card-img-top" src={women4} alt="" />
+          <div className="card" style={{width: '15rem', height:'75vh'}}>
+            <img className="card-img-top" src={shoe4} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Cat-Print Shirt</h5>
+            <h5 className="card-title">Desigual Black Ankle Boots</h5>
             <p></p>
-            <p className="card-text">Rs 2100/-</p>
-            <a href="#" className="btn">
-              Add To Cart
-            </a>
-            </div>
-          </div>
-          <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women5} alt="" />
-            <div className="card-body">
-            <h5 className="card-title">Split Joint Outerwear</h5>
-            <p></p>
-            <p className="card-text">Rs 2800/-</p>
+            <p className="card-text">Rs 7000/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women6} alt="" />
+            <img className="card-img-top" src={shoe5} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Denim mixed Jacket</h5>
+            <h5 className="card-title">Unisex Chunky Sneakers</h5>
             <p></p>
-            <p className="card-text">Rs 4500/-</p>
+            <p className="card-text">Rs 6500/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women7} alt="" />
+            <img className="card-img-top" src={shoe6} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Raw Edge Jeans</h5>
+            <h5 className="card-title">Ronnie Fieg for Clarks Originals</h5>
+            <p></p>
+            <p className="card-text">Rs 8000/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src={shoe7} alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Tehran Suede Patch Chunky Shoes</h5>
+            <p></p>
+            <p className="card-text">Rs 11000/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem', height:'75vh'}}>
+            <img className="card-img-top" src={shoe8} alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Tossu By Camper</h5>
             <p></p>
             <p className="card-text">Rs 2500/-</p>
             <a href="#" className="btn">
@@ -95,56 +106,45 @@ const Shoes = () => {
             </a>
             </div>
           </div>
-          <div className="card" style={{width: '15rem', height:'70vh'}}>
-            <img className="card-img-top" src={women8} alt="" />
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src={shoe9} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Wide Leg Jeans</h5>
+            <h5 className="card-title">ReebokClassics Club C 85 Vintage</h5>
             <p></p>
-            <p className="card-text">Rs 2500/-</p>
+            <p className="card-text">Rs 7000/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women9} alt="" />
+            <img className="card-img-top" src={shoe10} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Courduroy Crop Jacket </h5>
+            <h5 className="card-title">Adidas Yezzy Slides</h5>
             <p></p>
-            <p className="card-text">Rs 4000/-</p>
+            <p className="card-text">Rs 3500/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women10} alt="" />
+            <img className="card-img-top" src={shoe11} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Pathchwork Puffer</h5>
+            <h5 className="card-title">Custom Off-White X Air Jordan 1 Low</h5>
             <p></p>
-            <p className="card-text">Rs 4500/-</p>
+            <p className="card-text">Rs 8000/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
             </div>
           </div>
           <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women11} alt="" />
+            <img className="card-img-top" src={shoe12} alt="" />
             <div className="card-body">
-            <h5 className="card-title">Blouse Pants Sets</h5>
+            <h5 className="card-title">Double-Strap Buckled Platform Sandals</h5>
             <p></p>
-            <p className="card-text">Rs 6000/-</p>
-            <a href="#" className="btn">
-              Add To Cart
-            </a>
-            </div>
-          </div>
-          <div className="card" style={{width: '15rem'}}>
-            <img className="card-img-top" src={women12} alt="" />
-            <div className="card-body">
-            <h5 className="card-title">Trench Vest</h5>
-            <p></p>
-            <p className="card-text">Rs 3900/-</p>
+            <p className="card-text">Rs 6500/-</p>
             <a href="#" className="btn">
               Add To Cart
             </a>
