a3e6c51b9ed51684ee712d70729c8d310d87ed19
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-20 16:31:26 +0545

    replicating the cards from mens component to women component

--- styleit/src/components/women/Women.jsx
+++ styleit/src/components/women/Women.jsx
@@ -1,10 +1,150 @@
 import React from 'react'
+import women1 from '../../assets/women1.jpg'
+import women2 from '../../assets/women2.jpg'
+import women3 from '../../assets/women3.jpg'
+import women4 from '../../assets/women4.jpg'
 
 const Women = () => {
   return (
-    <div>
-      Hello Women
+    <section>
+    <div className='container Women'>
+    <h1>Women's Clothes</h1>
+        <div className="mens_items">
+          <div className="card" style={{width: '15rem', height:'70vh'}}>
+            <img className="card-img-top" src={women1} alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Summer Knit Shirt</h5>
+            <p></p>
+            <p className="card-text">Rs 3000/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src={women2} alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Lace Loose Shirt</h5>
+            <p></p>
+            <p className="card-text">Rs 2800/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src={women3} alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Brown Corduroy Pants</h5>
+            <p></p>
+            <p className="card-text">Rs 2500/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem', height:'70vh'}}>
+            <img className="card-img-top" src={women4} alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Linen SS Shirt Sand</h5>
+            <p></p>
+            <p className="card-text">Rs 2100/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Flower Denim Jacket</h5>
+            <p></p>
+            <p className="card-text">Rs 5000/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Bode Shirt</h5>
+            <p></p>
+            <p className="card-text">Rs 3200/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Knited Stirpped Shirt</h5>
+            <p></p>
+            <p className="card-text">Rs 2300/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem', height:'70vh'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Senior Cord</h5>
+            <p></p>
+            <p className="card-text">Rs 5500/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Paper pattern Denim </h5>
+            <p></p>
+            <p className="card-text">Rs 4000/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Colorful Pullover</h5>
+            <p></p>
+            <p className="card-text">Rs 3500/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">HipHop Sweatpants</h5>
+            <p></p>
+            <p className="card-text">Rs 2500/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+          <div className="card" style={{width: '15rem'}}>
+            <img className="card-img-top" src="" alt="" />
+            <div className="card-body">
+            <h5 className="card-title">Loose Fit  Sweatpants</h5>
+            <p></p>
+            <p className="card-text">Rs 2000/-</p>
+            <a href="#" className="btn">
+              Add To Cart
+            </a>
+            </div>
+          </div>
+        </div>      
     </div>
+    </section>
   )
 }
 
